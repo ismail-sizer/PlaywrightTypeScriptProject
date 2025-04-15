1a1cc62cd88be44515a746d5c6bf6d86b2678363
avena <ismail@retrace.ai>
2025-04-16 00:53:46 +0300

    test: assertions completed

--- tests/0941_assertionTest.spec.ts
+++ tests/0941_assertionTest.spec.ts
@@ -12,9 +12,9 @@ test('Visible/Hidden Assertion',async({page})=>{
     //npx playwright test 0941_assertionTest.spec.ts --headed --project=chromium
 })
 
-//0942 Assertions - Present/Not Present
+//0943 Assertions - Present/Not Present
 
-test.only('Present/Not Present Assertion',async({page})=>{
+test('Present/Not Present Assertion',async({page})=>{
     await page.goto('https://the-internet.herokuapp.com/add_remove_elements/');
     await expect(page.locator('.added-manually')).not.toHaveCount(1);
     await page.locator('text="Add Element"').click();
@@ -23,5 +23,73 @@ test.only('Present/Not Present Assertion',async({page})=>{
     await expect(page.locator('.added-manually')).not.toHaveCount(1);
     await page.close();
 
-    //npx playwright test 0941_assertionTest.spec.ts --headed --project=chromium
 })
+
+    //npx playwright test 0941_assertionTest.spec.ts --headed --project=chromium
+
+
+ //0944 Assertions - Enabled/Disabled
+
+ test('Enabled/Disabled Assertion',async({page})=>{
+    await page.goto('https://omayo.blogspot.com/');
+    await expect(page.locator('#but2')).toBeEnabled();
+    await expect(page.locator('#but1')).toBeDisabled();
+    await page.close();
+ 
+
+})
+
+//npx playwright test 0941_assertionTest.spec.ts --headed --project=chromium
+
+ //0945 Assertions - Text Match / Mismatch
+
+ test('Text Match / Mismatch Assertion',async({page})=>{
+    await page.goto('https://omayo.blogspot.com/');
+    await expect(page.locator('#but2')).toHaveText('Button2');
+    await expect(page.locator('#but1')).not.toHaveText('abcd');
+    await page.close();
+})
+
+//npx playwright test 0941_assertionTest.spec.ts --headed --project=chromium
+
+ //0946. Assertions - Element Attributes
+
+ test('Element Attributes Assertion',async({page})=>{
+    await page.goto('https://opensource-demo.orangehrmlive.com/');
+    await expect(page.locator('input[placeholder="Username"]')).toHaveAttribute('name','username');
+    await expect(page.locator('input[placeholder="Username"]')).toHaveAttribute('class',/.*oxd-input/);
+    await page.close();
+})
+
+//npx playwright test 0941_assertionTest.spec.ts --headed --project=chromium
+
+ //0947. Assertions - URL and Title
+
+ test('URL and Title Assertion',async({page})=>{
+    await page.goto('https://opensource-demo.orangehrmlive.com/');
+
+    //Full URL Assertion
+    await expect(page).toHaveURL('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login');
+
+    //Full URL Assertion
+    await expect(page).toHaveURL(/.*demo.orangehrmlive.com/);
+
+    //Full Title
+    await expect(page).toHaveTitle('OrangeHRM');
+
+    await page.close();
+})
+
+//npx playwright test 0941_assertionTest.spec.ts --headed --project=chromium
+
+
+ //0948. Assertions - Screenshot
+
+ test.only('Screenshot Assertion',async({page})=>{
+    await page.goto('https://opensource-demo.orangehrmlive.com/');
+    await page.waitForTimeout(2000);  // Wait for 2 seconds
+    await expect(page).toHaveScreenshot();
+    await page.close();
+})
+
+//npx playwright test 0941_assertionTest.spec.ts --headed --project=chromium
